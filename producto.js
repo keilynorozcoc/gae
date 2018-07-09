13169606c3efc9d8b4e540423d639046e95cfc51
keilynorozcoc <korozcoc@ucenfotec.ac.cr>
2018-07-08 21:48:17 -0600

    primeraVersion

---
+++ producto.js
@@ -0,0 +1,9 @@
+// funcion del estilo
+// $(document).ready(function(){
+//     $('[data-toggle="offcanvas"]').click(function(){
+//         $("#navigation").toggleClass("hidden-xs");
+//     });
+//  });
+//  funcion del estilo
+ 
+ 
